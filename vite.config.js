df5e5d6c10fa8c5e438a9f6faeab019d0118d3d4
Joshua Nanninga <jmarknanninga@gmail.com>
2024-09-26 20:32:50 -0500

    first try in netlify

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
+import reactDom from 'react-dom';
+
+export default {
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-icons/fa'],
   },
-});
+},
+};
