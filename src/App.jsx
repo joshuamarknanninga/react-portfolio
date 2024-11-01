b8f189682a05b16e69dfa3042aa91549ef89e75a
Joshua Nanninga <jmarknanninga@gmail.com>
2024-11-01 12:55:43 -0500

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,41 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Navigation from './components/Navigation';
 import About from './pages/About';
-import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
-// import BackgroundVideo from './components/BackgroundVideo';
-import './styles/App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-      {/* <BackgroundVideo /> Include the background video */}
+    <div className="min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white font-sans">
+      <Router>
         <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<About />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/resume" element={<Resume />} />
-          </Routes>
-        </main>
+        <Navigation />
+        
+        <motion.main
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8 }}
+          className="container mx-auto p-4"
+        >
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="/resume" element={<Resume />} />
+            </Routes>
+          </Suspense>
+        </motion.main>
+        
         <Footer />
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
